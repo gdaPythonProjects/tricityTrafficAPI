44e00414e0e2d0c9d00a4dec2a5bda274ffd7d14
Sinma <damianciftci@gmail.com>
2018-12-13 15:26:29 +0100

    Updated dataSourceModel

--- daemon/dataSourceModel.py
+++ daemon/dataSourceModel.py
@@ -1,8 +1,22 @@
 from abc import ABC, abstractmethod
+from shared.models import Notification
 
 
 class dataSourceModel(ABC):
+    def __init__(self, date=None):
+        self.notification_list = []
+        self.date = date
 
     @abstractmethod
-    def get(self, date=None):
+    def get_data(self):
         pass
+
+    @staticmethod
+    def create_notification(title, content, date, source):
+        notification = Notification()
+        notification.title = title
+        notification.content = content
+        notification.date = date
+        notification.source = source
+
+        return notification
