772b96b0cb373f6761a7a3c05ffa797b6c1ed3ad
Damian Ciftci <damianciftci@gmail.com>
2019-01-12 22:43:24 +0100

    Created dataSourcePuller

--- daemon/dataSourcePuller.py
+++ daemon/dataSourcePuller.py
@@ -1,4 +1,16 @@
 
 
 class DataSourcePuller:
-    pass
+    def __init__(self):
+        self.sources = [
+
+        ]
+
+    def pull_data(self):
+        for source in self.sources:
+            try:
+                s = source()
+                for notification in s.get_data():
+                    notification.save()
+            except:
+                pass
