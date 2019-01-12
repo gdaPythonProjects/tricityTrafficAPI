ef12658deee087f2f04210b51eca146533176c6b
Damian Ciftci <damianciftci@gmail.com>
2019-01-13 00:15:02 +0100

    Added __str__ function to notification

--- shared/models.py
+++ shared/models.py
@@ -13,3 +13,6 @@ class Notification(models.Model):
     content = models.TextField()
     date = models.DateTimeField(default=timezone.now)
     source = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"({self.date}) {self.title}"
